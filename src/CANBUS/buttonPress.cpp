498cc5d8801cb4dcd2deabf4741564c5c40b23fd
alexvnesta <alex.v.nesta@gmail.com>
2022-04-16 00:36:56 -0400

    cleaned up double click logic

--- src/CANBUS/buttonPress.cpp
+++ src/CANBUS/buttonPress.cpp
@@ -9,61 +9,50 @@ unsigned volatile long lastButtonPressTime = 0;
 
 int volatile numClicks = 0;
 
-
 void parseCanInterruptedMessage(uint32_t id, const uint8_t message[], uint8_t messageLength){
     if (id == STEERING_WHEEL_ID){
     Serial.println("STEERING WHEEL BUTTON!!!!!");   
-        // IF the particular voice button is pressed...
-        if (message[0] == 192 && message[1] == 13 && (millis() - lastButtonPressTime > 150)){
-          lastButtonPressTime = millis();
-          Serial.println("MATCHES VOICE BUTTON!");
-          numClicks = checkNumClicks();
+        if (message[0] == 192 && message[1] == 13){ // IF the particular voice button is pressed...
+          lastButtonPressTime = millis();  // Reset button press timer
+          numClicks = checkNumClicks(); // get number of clicks
 
             if(numClicks == 2){
-              Serial.println("DOUBLE ClICK!");
+              Serial.println("DOUBLE CLICK!");
+              // DO SOME ACTION TO THE SCREEN
             }
             else if(numClicks == 1){
-              //Single click event
-              Serial.println("SINGLE ClICK!");
-              lastButtonPress = millis();
-              // CALL THE FUNCTION TO SWITCH THE SCREEN!                 
+              Serial.println("SINGLE CLICK!");
+              // CALL THE FUNCTION TO SWITCH THE SCREEN!
             }
-          
-        }
+          }
     }
 }
 
-
 int checkNumClicks(void){
-    static unsigned long max_delay = 800;
-    int returnVal = 0;
-    Serial.print("before Adding to Counter");
-    Serial.println(clickCounter);
-    clickCounter += 1;
-    Serial.print("After Adding to Counter");
-    Serial.println(clickCounter);
+  static int max_delay = 800;
+  int returnVal = 0;
+  Serial.print("before Adding to Counter");
+  Serial.println(clickCounter);
+  clickCounter += 1;
+  Serial.print("After Adding to Counter");
+  Serial.println(clickCounter);
 
-    lastButtonPress = millis();
-
-    while((millis() - lastButtonPress < max_delay) && clickCounter == 1){
-        can1.events();
-        if (clickCounter == 2){
-        //Serial.println("Button pressed twice!");
+  while((millis() - lastButtonPress < max_delay) && clickCounter == 1){
+      can1.events();  //keep getting can messages so that the whole device does not wait on this next button press
+      if (clickCounter == 2){
         returnVal = 2;
         clickCounter = 0;
-        lastButtonPress = millis();
-        break;
-        return(returnVal);
-        }
-    }
-    if (clickCounter == 1){
-        //Serial.println("Button pressed once!");
-        clickCounter = 0;
-        returnVal = 1;
-        lastButtonPress = millis();
-        return(returnVal);
-    }
-
-    return (returnVal);
+      break;
+      return(returnVal);
+      }
+  }
+  if ((clickCounter == 1) && (millis() - lastButtonPress >= max_delay)){
+      clickCounter = 0;
+      returnVal = 1;
+      lastButtonPress = millis();
+      return(returnVal);
+  }
+  clickCounter = 0;
+  return (returnVal);
 }
 
